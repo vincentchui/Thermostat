ac9342fc41337a364d0c8784d829b00be100f4e4
vincentchui <vincentchuicalifornia@gmail.com>
2015-11-01 09:35:20 -0800

    initial

---
+++ Thermostat.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thermostat
+{
+    class Thermostat
+    {
+        private int min;
+        private int max;
+
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                min = value;
+            }
+        }
+
+        public int Max
+        {
+            get
+            {
+                return max;
+            }
+            set
+            {
+                max = value;
+            }
+        }
+
+        public Thermostat(int min, int max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public void SetTemp(int t)
+        {
+            if(t < Min)
+            {
+                throw new System.ArgumentException("Temperature is too cold!");
+            }
+            else if(t > Max)
+            {
+                throw new System.ArgumentException("Temperature is too hot!");
+            }
+            else
+            {
+                Console.WriteLine("\nTemperature is OK!");
+            }
+        } 
+
+        public TemperatureOutofRange(int t)
+        {
+            if (t > Max)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ ThermostatTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thermostat
+{
+    class ThermostatTester
+    {
+        static void Main(string[] args)
+        {
+            Thermostat t = new Thermostat(0, 100);
+        }
+    }
+}
